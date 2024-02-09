df4b4b78838c77b020d19e4a79ba4fa2582b1e19
Steve Chambers <steve.chambers@viewyonder.com>
2024-02-09 15:47:16 +0000

    Initial commit - just header statements.

---
+++ viewyonder.php
@@ -0,0 +1,28 @@
+/*
+ * Plugin Name:       Viewyonder Plugin
+ * Plugin URI:        https://viewyonder.com/plugins/my-plugin/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Author:            Steve Chambers
+ * Author URI:        https://viewyonder.com/about/steve-chambers
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://viewyonder.com/my-plugin/
+ * Text Domain:       viewyonder-plugin
+ * Domain Path:       /languages
+ */
+
+/*
+Viewyonder Plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Viewyonder Plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Viewyonder Plugin. If not, see {URI to Plugin License}.
+*/
